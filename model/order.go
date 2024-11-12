825b62c5bdec0e0f9bd247d49ff4a645994413c4
xiaominghao <xiaominghao@baidu.com>
2024-11-12 21:45:02 +0800

    feat: update table schema
    Signed-off-by: xiaominghao <xiaominghao@baidu.com>

--- model/order.go
+++ model/order.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Order struct {
-	ID        int64     `gorm:"primaryKey;autoIncrement;comment:订单ID"`
-	UID       int64     `gorm:"not null;comment:用户ID;column:uid"`
-	Weight    float64   `gorm:"not null;comment:重量(kg);column:weight"`
+	ID        int64     `gorm:"primaryKey;autoIncrement;type:bigint;comment:订单ID"`
+	UID       int64     `gorm:"type:bigint;not null;comment:用户ID;column:uid"`
+	Weight    float64   `gorm:"type:double;not null;comment:重量(kg);column:weight"`
 	CreatedAt time.Time `gorm:"type:datetime;not null;default:CURRENT_TIMESTAMP;comment:创建时间;column:created_at"`
 }
 
